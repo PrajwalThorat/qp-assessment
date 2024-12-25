4f1ce423ead0b3027ccd10a0883e39f0a4c9f6eb
Prajwal Thorat <prajwal.thorat@bnedloudcloud.com>
2024-12-25 23:55:13 +0530

    code changes

---
+++ src/main/java/com/groceryapp/qp_assessment/GroceryAppApplication.java
@@ -0,0 +1,13 @@
+package com.groceryapp.qp_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groceryapp/qp_assessment/GroceryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groceryapp.qp_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
